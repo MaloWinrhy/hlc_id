27eaad30e9592235aa362eef621128cc362ea7ba
Winrhy <malo@arkada.fr>
2024-12-18 14:12:59 +0100

    feat: :sparkles: Imp HybridLogicalClock

--- src/clock.rs
+++ src/clock.rs
@@ -2,3 +2,17 @@ pub struct HybridLogicalClock {
     timestamp: u64,
     node_id: u16,
 }
+
+impl HybridLogicalClock {
+
+    pub fn new(node_id : u16) -> HybridLogicalClock {
+        HybridLogicalClock {
+            timestamp: 0,
+            node_id: node_id,
+        }
+    }
+
+    pub fn current_timestamp(&self) -> u64 {
+        self.timestamp
+    }
+}
