b319ac229e3222b6138b0dd4329e0d9f7fa2c6cf
Winrhy <malo@arkada.fr>
2024-12-18 14:14:02 +0100

    feat: :sparkles: init clock in lib.rs

--- src/clock.rs
+++ src/clock.rs
@@ -8,7 +8,7 @@ impl HybridLogicalClock {
     pub fn new(node_id : u16) -> HybridLogicalClock {
         HybridLogicalClock {
             timestamp: 0,
-            node_id: node_id,
+            node_id,
         }
     }
 
